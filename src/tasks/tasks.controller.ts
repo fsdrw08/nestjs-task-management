4a1da44ec2a50e98cc1f3cab5cba97238fdfde17
Windom Wu <windomwu@MacdeMBP-4.wirelessdomain>
2019-10-09 23:42:49 +0800

    create task service and controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './task.modle';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {}
 
-    @Get()
-    getAllTasks() {
-        return this.tasksService.getAllTasks();
-    }
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 
-    @Post()
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,24 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.modle';
 import * as uuid from 'uuid/v1';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
-    private tasks: Task[] = [];
+  private tasks: Task[] = [];
 
-    getAllTasks():Task[] {
-        return this.tasks;
-    }
+  getAllTasks(): Task[] {
+      return this.tasks;
+  }
 
-    createTask(title: string, description: string):Task {
-	const task:Task = {
-		id: uuid(),
-		title,
-		description,
-		status: TaskStatus.OPEN,
-	};
-	
-	this.tasks.push(task);
-	return task;
-    }
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto; // desctruction
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
 }
