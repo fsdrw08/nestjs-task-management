be0810ada9a98586ba8e45d91ae54aea8bee441d
Windom Wu <windomwu@Windoms-MBP.localdomain>
2019-10-16 23:16:42 +0800

    Set up custom pipe to validating the task status

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,25 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../task.modle';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.DONE,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.OPEN,
+  ];
+
+  private isStatusValue(status: any): boolean {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValue(value)) {
+      throw new BadRequestException(`the status "${value}" is invalid`);
+    }
+
+    return value;
+  }
+}
