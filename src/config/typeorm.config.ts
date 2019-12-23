e2224af938ca675d366afce36f857ce928caa3b9
Windom Wu <windomwu@Windoms-MacBook.local>
2019-12-23 22:15:37 +0800

    modify typeorm config according to docker-compose content

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'db',
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'taskmanagement',
+  username: 'user',
+  password: 'pass',
+  database: 'data',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
