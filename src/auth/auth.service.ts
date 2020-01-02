7578b3a780bcfcd5db98374ad85eb04747fc06a6
root <drw_08@hotmail.com>
2020-01-02 08:53:49 +0000

    a raw signup feature

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
-  constructor (
+  constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    await this.userRepository.signUp(authCredentialsDto);
+  }
 }
