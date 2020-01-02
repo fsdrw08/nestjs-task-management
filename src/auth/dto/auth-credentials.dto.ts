3910cffb7d580f8002784aeba843cf043dc15e63
root <drw_08@hotmail.com>
2020-01-02 10:13:39 +0000

    set up username and password base validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,19 @@
+import { IsString, IsNotEmpty, MaxLength, MinLength, Matches } from 'class-validator';
+
 export class AuthCredentialsDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(2)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    @MaxLength(20)
+    @Matches(
+        /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+        {message: 'password too weak'},
+    )
     password: string;
 }
